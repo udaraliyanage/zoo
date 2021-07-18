3b786af93e87b115bf7c93c6e1e5b7820e2aa489
udaraliyanage <udaraliyanage@gmail.com>
2021-07-18 21:11:34 +0800

    [Refac] Bird implements Flyable

--- src/main/java/com/udara/zoo/model/Bird.java
+++ src/main/java/com/udara/zoo/model/Bird.java
@@ -1,16 +1,18 @@
 package com.udara.zoo.model;
 
 
+import com.udara.zoo.model.behavior.Flyable;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public abstract class Bird extends WalkingAnimal{
+public abstract class Bird extends WalkingAnimal implements Flyable {
 
     public Bird(String name) {
         super(name);
     }
 
-    void fly() {
+    @Override
+    public void fly() {
         System.out.println("I am flying");
     }
 }
